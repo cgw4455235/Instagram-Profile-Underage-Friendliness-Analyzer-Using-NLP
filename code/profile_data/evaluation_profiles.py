fb894f415331638e20f63a7ed66a17f97b8bb49f
cgw4455235 <wcg4455@gmail.com>
2023-12-09 14:25:55 -0500

    Create evaluation_profiles.py

---
+++ code/profile_data/evaluation_profiles.py
@@ -0,0 +1,38 @@
+violent_evaluation_profiles = [
+    "_history_of_violence_",
+    "vintage.mma",
+    "nochildreninwar",
+    "stop._.animal_cruelty",
+    "__stop.animal.cruelty__",
+    "prevent.animal.cruelty",
+    "violencestories",
+    "guntrends",
+    "guns.lover",
+    "frontier_conflict",
+]
+
+negative_evaluation_profiles = [
+    "healing.after.a.narcissist",
+    "wizard_of_radical_self_respect",
+    "depression.quotes.sad",
+    "healingmyheart101",
+    "unitednationshumanrights",
+    "victimblamed",
+    "anxiety_wellbeing",
+    "psych.chat",
+    "graceisunderfire",
+    "literature_underground",
+]
+
+educational_evaluation_profiles = [
+    "hilaryjacobshendel",
+    "yourjourneythrough",
+    "scimyth",
+    "thelearningexperience",
+    "history_and_stories",
+    "history.lessons",
+    "coders.learning",
+    "bigthinkers",
+    "neatoday",
+    "facts_of_physiotherapy",
+]
